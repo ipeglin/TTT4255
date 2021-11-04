f6fd97bc249437c6d938af6ba6c824deb284b4f3
Ian Philip Eglin <philip@eglin.no>
2021-11-04 17:52:45 +0100

    Corrected pwd path for every file when cp

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Navigating to correct repo directory on the RP
-cd "/home/pi/Documents/Git/TTT4255---Elsys-GK-Innovasjonsprosjekt---Raspberry-Pi"
+cd "/home/pi/Documents/Git/TTT4255---Elsys-GK-Innovasjonsprosjekt---Raspberry-Pi/"
 
 # Pulling the newest version from the remote GitHub repo
 git pull origin master
 
 # Copying the files to the users home directory so they can be used without navigating to repo dir.
-sudo cp "./*" ~
+sudo cp * ~
