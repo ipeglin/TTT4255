54520b4c6a7d70355b5e5cdaed40cc57ce3b4dc0
ipeglin <philip@eglin.no>
2021-11-04 17:12:21 +0100

    Added the ability to modofy the white balance by using as the  argument when running the launch.sh script

--- launch.sh
+++ launch.sh
@@ -1,8 +1,15 @@
+#!/bin/bash
+
 cd ~/Documents/Git/TTT4255---Elsys-GK-Innovasjonsprosjekt---Raspberry-Pi/
+echo "You chose White Balance Mode: $1"
 sudo raspistill \
 	--fullscreen \
-	--timeout 0 \
-	--quality 100 \
+	--vstab \
 	--keypress \
+	--timeout 0 \
+	--flicker 60hz \
+	--encoding png \
+	-awb $1 \
+	--quality 100 \
 	-ISO 800 \
 	--saturation -5
