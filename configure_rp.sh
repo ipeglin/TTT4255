84a5ce314523975bdec2d7f6c0a4bc9f1933d746
ipeglin <philip@eglin.no>
2021-11-04 14:16:02 +0100

    Added terminal history from RP

---
+++ configure_rp.sh
@@ -0,0 +1,21 @@
+SUDO RASPI/CONFIG
+sudo raspi-congig
+sudo raspi-config
+raspistill -o Desktop/image.jpg
+vcgencmd get_camera
+raspistill
+raspistill -p
+raspivid -p
+raspivid
+raspivid -o
+raspivid -o Desktop/video.h264
+sudo halt
+history
+raspistill -f -t 0
+sudo raspi-config
+raspistill -f -t 0
+raspistill
+raspistill -f -t 0
+sudo halt
+history
+
