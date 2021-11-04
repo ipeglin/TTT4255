9ffa11a0e7bfa29d3aeb71407c8f8a1351d86b28
ipeglin <philip@eglin.no>
2021-11-04 16:28:56 +0100

    Added shell scripts that launch the camera with a simple command and another script that updates the github repo by pulling from remote

---
+++ launch.sh
@@ -0,0 +1,8 @@
+cd ~/Documents/Git/TTT4255---Elsys-GK-Innovasjonsprosjekt---Raspberry-Pi/
+sudo raspistill \
+	--fullscreen \
+	--timeout 0 \
+	--quality 100 \
+	--keypress \
+	-ISO 800 \
+	--saturation -5
---
+++ update.sh
@@ -0,0 +1,2 @@
+cd "~/Documents/Git/TTT4255---Elsys-GK-Innovasjonsprosjekt---Raspberry-Pi"
+git pull origin master
